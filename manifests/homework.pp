f8be2975a36dfcad474a1cb5a9fdcc1584cc4713
Patrick Kelso <psk@puppetlabs.com>
2015-02-16 20:13:32 +1100

    databases

--- manifests/homework.pp
+++ manifests/homework.pp
@@ -8,4 +8,9 @@ class roles::homework {
   include profiles::vhost3
   include profiles::vhost4
   include profiles::vhost5
+  include profiles::mysqldb1
+  include profiles::mysqldb2
+  include profiles::mysqldb3
+  include profiles::mysqldb4
+  include profiles::mysqldb5
 }
